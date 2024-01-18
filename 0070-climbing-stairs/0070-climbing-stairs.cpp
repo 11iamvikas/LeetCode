2e2169cc9a06eaf51265fec7e20314c90b2dd9bf
Vikas Pant <123629697+11iamvikas@users.noreply.github.com>
2024-01-18 15:00:59 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (35.64%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        int x =1, y =2;
+        for(int i =3; i<n; i++){
+            int tmp = y;
+            y += x;
+            x = tmp;
+        }
+        return(x+y);
+    }
+};
