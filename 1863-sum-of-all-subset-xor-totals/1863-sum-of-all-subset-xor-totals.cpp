32746bb867957a583f4102ff2584762006a815ea
Vikas Pant <123629697+11iamvikas@users.noreply.github.com>
2024-05-21 20:50:13 +0530

    Time: 3 ms (68.96%), Space: 8.3 MB (65.21%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        // First create helper function, this function will help us to calculate
+        // when I'm including, when I'm not. The XOR of that array
+        return helper(nums, 0, 0); // nums, level, current XOR
+    }
+
+private:
+    int helper(vector<int>& nums, int level, int currentXOR) {
+        // base condition
+        if (level == nums.size()) return currentXOR;
+        // creating include 
+        int include = helper(nums, level + 1, currentXOR ^ nums[level]);
+        // creating exclude
+        int exclude = helper(nums, level + 1, currentXOR);
+        
+        return include + exclude;
+    }
+};
