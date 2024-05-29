7dc8bcb816e83f3af1e068dd3ca756938609b0cf
Vikas Pant <123629697+11iamvikas@users.noreply.github.com>
2024-05-29 22:10:50 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (25.06%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int l = s.length() - 1;
+        int carry = 0;
+        int count = 0;
+        
+        while (l > 0) {
+            // even number with carry = 0, result even
+            if (s[l] - '0' + carry == 0) {
+                carry = 0;
+                count++;
+            // odd number with carry = 1, result even
+            } else if (s[l] - '0' + carry == 2) {
+                carry = 1;
+                count++;
+            // even number with carry = 1, result odd
+            // odd number with carry = 0, result odd
+            } else {
+                carry = 1;
+                count += 2;
+            }
+            l--;
+        }
+        
+        // last digit 1 needs to add a carried over digit 1, which gives 10.
+        // So need one more divide to make it 1 (one more step)
+        if (carry == 1) {
+            count++;
+        }
+        
+        return count;
+    }
+};
